a59922354cb86aba2855dfb5ab74441f51b46ccd
saowalak.tb@rmuti.ac.th <b0613384510>
2020-07-31 19:45:41 +0700

    Add Class

--- app/src/main/java/com/example/firstapp/AbouteFragment.kt
+++ app/src/main/java/com/example/firstapp/AbouteFragment.kt
@@ -1,4 +1,16 @@
 package com.example.firstapp
 
-class AbouteFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AboutFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
 }
+
