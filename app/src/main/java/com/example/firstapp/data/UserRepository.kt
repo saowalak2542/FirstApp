63160660c579396b5e808522c05e9068e3a36986
saowalak2542 <saowalak.tb@rmuti.ac.th>
2020-08-16 21:37:02 +0700

    AddUserRepository

---
+++ app/src/main/java/com/example/firstapp/data/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.example.firstapp.data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository (private val userDao: UserDao){
+
+    val readAllData: LiveData<List<User>> = userDao.reaadAllData()
+
+   suspend fun  addUser(user: User){
+        userDao.addUser(user)
+    }
+}
